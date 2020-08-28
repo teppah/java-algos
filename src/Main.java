96bd5975c62101844b3bf0474ce44177ed355b57
Yi Feng Yang <yifeng@yfyang.dev>
2020-08-28 12:32:49 -0400

    Implement quicksort

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
+    private static final int[] intArray = {44, 42, 55, 82, 13, 767, 5, 56, 8, 3, 6, 86, 58, 23, 3, 5433, 654, 234, 5, 5435, 345, 68546, 345544, 4, 43243, 4, 33, 22, 6564, 5233};
+
     public static void main(String[] args) {
-        int[] toSort = {44, 3, 5433, 654, 234, 5, 5435};
+        testSelectionSort();
+        testQuicksort();
+    }
+
+    public static void testSelectionSort() {
+        int[] toSort = Arrays.copyOf(intArray, intArray.length);
         SelectionSort.sort(toSort);
-        System.out.println(Arrays.toString(toSort));
+        System.out.println("Selection sort:\n" + Arrays.toString(toSort));
+    }
+
+    public static void testQuicksort() {
+        List<Integer> toSort = Arrays
+                .stream(intArray)
+                .boxed()
+                .collect(Collectors.toCollection(ArrayList::new));
+        List<Integer> sorted = Quicksort.sort(toSort);
+        System.out.println("Quicksort:\n" + sorted);
     }
 }
--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Quicksort {
     public static List<Integer> sort(List<Integer> array) {
+        if (array.size() < 2) {
+            return array;
+        }
+        int midIndex = array.size() / 2;
+        int middle = array.remove(midIndex);
+        List<Integer> smaller = array
+                .stream()
+                .filter(val -> val <= middle)
+                .collect(Collectors.toCollection(ArrayList::new));
+        List<Integer> bigger = array
+                .stream()
+                .filter(val -> val > middle)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        List<Integer> sortedSmaller = sort(smaller);
+        List<Integer> sortedBigger = sort(bigger);
+
+        List<Integer> result = new ArrayList<>(sortedSmaller);
+        result.add(middle);
+        result.addAll(sortedBigger);
+        return result;
     }
 }
